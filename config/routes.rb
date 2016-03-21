911ee2978e9f4f31aa509ce774a5bef650d58f63
chrwog <wongachris@gmail.com>
2016-03-20 23:20:32 -0700

    Add scaffold files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  # get '/posts' => 'posts#index'
+  # get '/posts/:id' => 'posts#show'
+  # get '/posts/new' => 'posts#new'
+  # get '/posts/:id/edit' => 'posts#edit'
+  # post '/posts' => 'posts#create'
+  # patch '/posts/:id' => 'posts#update'
+  # delete '/posts/:id' => 'posts#destroy'
+
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
